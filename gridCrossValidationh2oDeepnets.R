a2b1e59640a28654d3b0d8e781d1b16f4c1281b4
wacax <wacax@hotmail.com>
2014-10-01 16:10:21 -0500

    updated validation

--- gridCrossValidationh2oDeepnets.R
+++ gridCrossValidationh2oDeepnets.R
@@ -13,6 +13,8 @@ gridCrossValidationh2oDeepnets <- function(H2OParsedDataObject, noOfEpochs = 5, 
                    'Rectifier', 'Tanh', 'Maxout')
   targetsVal <- cbind(as.data.frame(HexValid$Ca), as.data.frame(HexValid$P), as.data.frame(HexValid$pH), 
                       as.data.frame(HexValid$SOC), as.data.frame(HexValid$Sand))
+  targetsTest <- cbind(as.data.frame(HexTest$Ca), as.data.frame(HexTest$P), as.data.frame(HexTest$pH), 
+                       as.data.frame(HexTest$SOC), as.data.frame(HexTest$Sand))
   
   noDropout <- c('Rectifier', 'Tanh', 'Maxout')
   
@@ -22,8 +24,9 @@ gridCrossValidationh2oDeepnets <- function(H2OParsedDataObject, noOfEpochs = 5, 
                                 y = target,
                                 data = HexTrain,
                                 classification = FALSE, balance_classes = FALSE, 
+                                validation = HexValid, 
                                 activation = actvs,
-                                hidden = c(20, 20),
+                                hidden = c(50, 50),
                                 input_dropout_ratio = ifelse(actvs %in% noDropout, 0, 0.1),
                                 l2 = ifelse(actvs == 'Rectifier' | actvs == 'Tanh', 1e-5, 0),
                                 epochs = noOfEpochs, force_load_balance = TRUE)  
@@ -37,7 +40,7 @@ gridCrossValidationh2oDeepnets <- function(H2OParsedDataObject, noOfEpochs = 5, 
   })
   
   #Create a set of network topologies
-  hidden_layers = list(c(50, 50), c(50, 50), c(50, 50, 50), c(50, 100, 50), c(50, 50, 50, 50))
+  hidden_layers = list(c(50, 50), c(100, 100), c(50, 50, 50), c(100, 100, 100))
   
   optimalParameters <- cbind(predCol, optimalActivations)
   
@@ -47,6 +50,7 @@ gridCrossValidationh2oDeepnets <- function(H2OParsedDataObject, noOfEpochs = 5, 
                                 y = parameters[1],
                                 data = HexTrain,
                                 classification = FALSE, balance_classes = FALSE, 
+                                validation = HexValid, 
                                 activation = parameters[2],
                                 hidden = architecture,
                                 input_dropout_ratio = ifelse(parameters[2] %in% noDropout, 0, 0.1),
@@ -63,21 +67,23 @@ gridCrossValidationh2oDeepnets <- function(H2OParsedDataObject, noOfEpochs = 5, 
   })
   
   optimalParameters <- cbind(optimalParameters, optimalArchitecture)
-  return(optimalParameters)
   
   #ADADELTA
-  adaRhos <- c(0.9, 0.95, 0.99)
-  optimalAdaRhos <- apply(optimalParameters, 1, function(parameters){
-    activationsErrors <- sapply(adaRhos, function(rho){      
+  #grid search
+  gridAda <- expand.grid(c(0.9, 0.95, 0.99), c(1e-15, 1e-12, 1e-10, 1e-8, 1e-6), stringsAsFactors = TRUE) #this creates all possible combinations
+  
+  optimalAdaParams <- apply(optimalParameters, 1, function(parameters){
+    activationsErrors <- apply(gridAda, 1, function(adaDelta){      
       model <- h2o.deeplearning(x = seq(2, 3595),
                                 y = parameters[1],
                                 data = HexTrain,
-                                classification = FALSE, balance_classes = FALSE, 
+                                classification = FALSE, balance_classes = FALSE,
+                                validation = HexValid,                                 
                                 activation = parameters[2],
                                 hidden = hidden_layers[[as.numeric(parameters[3])]],
                                 adaptive_rate = TRUE,
-                                rho = rho,
-                                epsilon = 1e-10,
+                                rho = as.numeric(adaDelta[1]),
+                                epsilon = as.numeric(adaDelta[2]),
                                 input_dropout_ratio = ifelse(parameters[2] %in% noDropout, 0, 0.1),
                                 l2 = ifelse(parameters[2] == 'Rectifier' | parameters[2] == 'Tanh', 1e-5, 0),
                                 epochs = noOfEpochs * 2, force_load_balance = TRUE)  
@@ -86,37 +92,43 @@ gridCrossValidationh2oDeepnets <- function(H2OParsedDataObject, noOfEpochs = 5, 
       RMSEError <- rmse(targetsVal[parameters[1]], Prediction)
       print(paste0(parameters[1], ' RMSE Error of ', RMSEError, ' with activation: ', parameters[2], 
                    ' and ', length(hidden_layers[[as.numeric(parameters[3])]]), ' hidden layers of ',
-                   hidden_layers[[as.numeric(parameters[3])]], ' units each. Adadelta rho of ', rho))
+                   hidden_layers[[as.numeric(parameters[3])]], ' units each. Adadelta rho of ', as.numeric(adaDelta[1]), 
+                   ' and Epsilon of: ', as.numeric(adaDelta[2])))
       return(RMSEError)
     })    
     return(which.min(activationsErrors))    
   })
   
-  optimalParameters <- cbind(optimalParameters, optimalAdaRhos)
+  optimalParameters <- cbind(optimalParameters, optimalAdaParams)
+  
+  #Compute Combined Score
+  #Use 80% of data and 20% of test scores
+  Hex80 <- H2OParsedDataObject[1:floor(dim(H2OParsedDataObject)[1] * 0.8), ]
   
   if(printScore == TRUE){
     RMSEs <- apply(optimalParameters, 1, function(parameters){
       model <- h2o.deeplearning(x = seq(2, 3595),
                                 y = parameters[1],
-                                data = HexTrain,
+                                data = Hex80,
                                 classification = FALSE, balance_classes = FALSE, 
                                 activation = parameters[2],
                                 hidden = hidden_layers[[as.numeric(parameters[3])]],
                                 adaptive_rate = TRUE,
-                                rho = adaRhos[[as.numeric(parameters[4])]],
-                                epsilon = 1e-10,
+                                rho = gridAda[as.numeric(parameters[4]), 1],
+                                epsilon = gridAda[as.numeric(parameters[4]), 2],
                                 input_dropout_ratio = ifelse(parameters[2] %in% noDropout, 0, 0.1),
                                 l2 = ifelse(parameters[2] == 'Rectifier' | parameters[2] == 'Tanh', 1e-5, 0),
-                                epochs = noOfEpochs * 2, force_load_balance = TRUE)  
+                                epochs = noOfEpochs * 4, force_load_balance = TRUE)  
       
-      Prediction <- unlist(as.data.frame(h2o.predict(model, newdata = HexValid)))
-      RMSEError <- rmse(targetsVal[parameters[1]], Prediction)
+      Prediction <- unlist(as.data.frame(h2o.predict(model, newdata = HexTest)))
+      RMSEError <- rmse(targetsTest[parameters[1]], Prediction)
       print(paste0(parameters[1], ' RMSE Error of ', RMSEError, ' with activation: ', parameters[2], 
                    ' and ', length(hidden_layers[[as.numeric(parameters[3])]]), ' hidden layers of ',
-                   hidden_layers[[as.numeric(parameters[3])]], ' units each. Adadelta rho of ', rho))
+                   hidden_layers[[as.numeric(parameters[3])]], ' units each. Adadelta rho of ', gridAda[as.numeric(parameters[4]), 1], 
+                   ' and Epsilon of: ', gridAda[as.numeric(parameters[4]), 2]))
       return(RMSEError)
     })  
-    print(paste0('MERMSE Score of: ', mean(RMSEs)))
+    print(paste0('MCRMSE Score of: ', mean(RMSEs)))
   }
   
   return(optimalParameters)
